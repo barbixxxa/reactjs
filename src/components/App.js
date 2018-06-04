34064e949c8757f2a46b820bb1e11618ac4e6caf
theeam <eduuardomuller@gmail.com>
2018-06-04 16:34:39 -0300

    parte 5

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Contador from './Contador'
 
 export default class App extends React.Component {
 
     render() {
-        return <h1>Hello React!</h1>
+        return (
+            <Contador />
+        );
     }
 }
