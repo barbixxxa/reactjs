054309f6fc2a2b11108ec4d86ffafe03954e5b71
theeam <eduuardomuller@gmail.com>
2018-06-06 08:53:38 -0300

    next lesson: taking turns

---
+++ reactjsorg/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Game from './Game';
+import Board from './components/Board';
+import Square from './components/Square';
+
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
